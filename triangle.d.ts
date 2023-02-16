429c229c69f22e1af11f5d74097b1559341f027c
3Dlyra <3dlyra.com@gmail.com>
2023-02-16 21:20:22 +0500

    Implement `getNormal` method

--- triangle.d.ts
+++ triangle.d.ts
@@ -1,1 +1,2 @@
 export function create( a?: Float32Array, b?: Float32Array, c?: Float32Array ): Float32Array;
+export function getNormal( triangle: Float32Array ): Float32Array;
--- triangle.js
+++ triangle.js
@@ -10,3 +10,26 @@ export function create( a, b, c ) {
 		...c
 	] )
 }
+
+export function getNormal( triangle ) {
+
+	const normal = new Float32Array( [
+		( triangle[ 7 ] - triangle[ 4 ] ) * ( triangle[ 2 ] - triangle[ 5 ] ) - ( triangle[ 8 ] - triangle[ 5 ] ) * ( triangle[ 1 ] - triangle[ 4 ] ),
+		( triangle[ 8 ] - triangle[ 5 ] ) * ( triangle[ 0 ] - triangle[ 3 ] ) - ( triangle[ 6 ] - triangle[ 3 ] ) * ( triangle[ 2 ] - triangle[ 5 ] ),
+		( triangle[ 6 ] - triangle[ 3 ] ) * ( triangle[ 1 ] - triangle[ 4 ] ) - ( triangle[ 7 ] - triangle[ 4 ] ) * ( triangle[ 0 ] - triangle[ 3 ] )
+	] )
+
+	const len = normal[ 0 ] * normal[ 0 ] + normal[ 1 ] * normal[ 1 ] + normal[ 2 ] * normal[ 2 ]
+
+	if ( len > 0 ) {
+
+		normal[ 0 ] = normal[ 0 ] * 1 / Math.sqrt( len )
+		normal[ 1 ] = normal[ 1 ] * 1 / Math.sqrt( len )
+		normal[ 2 ] = normal[ 2 ] * 1 / Math.sqrt( len )
+
+		return normal
+	}
+	else {
+		return new Float32Array( [ 0, 0, 0 ] )
+	}
+}
